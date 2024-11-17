44e392cfd467de88ea9922450a7ccffe5fa5084e
Cxx-mlr <spapiernik12@gmail.com>
2024-11-17 14:31:56 -0500

    :sparkles: Implement Lab 1 exercises

---
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_1/Program_2.py
@@ -0,0 +1,17 @@
+from rich import print
+from pathlib import Path
+import re
+
+def main():
+    print(f"[green]Programa 2[/]\n")
+
+    with open(Path(__file__).parent / "test_pr2.txt", "rt", encoding="utf-8") as fp:
+        paragraphs = fp.read().split("\n\n")
+
+        for paragraph_index, paragraph in enumerate(paragraphs, start=1):
+            count = len(re.findall(r"\ben\b", paragraph, re.IGNORECASE))
+            print(f"[bold cyan]Párrafo #{paragraph_index}[/]")
+            print(f"La palabra [bold green]\"en\"[/] se repite [bold green]{count}[/] veces\n")
+
+if __name__ == "__main__":
+    main()
---
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_1/Program_3.py
@@ -0,0 +1,37 @@
+from rich import print
+
+MAX_ATTEMPS = 3
+
+ACCESS_GRANTED_MESSAGE = "[green]Acceso permitido[/]"
+ACCESS_DENIED_MESSAGE = "[yellow]Datos incorrectos[/]"
+ACCESS_BLOCKED_MESSAGE = "[red]Lo siento, su acceso no es permitido[/]"
+
+fake_db = {
+    "Juan1223": "J12an*.",
+    "Maria2345": "M23a*.",
+    "Pablo1459": "P14o*.",
+    "Ana3456": "A34a*"
+}
+
+def main():
+    print(f"[green]Programa 3[/]\n")
+
+    attempts_left = MAX_ATTEMPS
+
+    while attempts_left > 0:
+        attempts_left -= 1
+
+        username = input("username: ")
+        password = input("password: ")
+
+        if not username in fake_db or fake_db[username] != password:
+            print(ACCESS_DENIED_MESSAGE, end="\n\n")
+            continue
+        else:
+            print(ACCESS_GRANTED_MESSAGE)
+            break
+    else:
+        print(ACCESS_BLOCKED_MESSAGE)
+
+if __name__ == "__main__":
+    main()
