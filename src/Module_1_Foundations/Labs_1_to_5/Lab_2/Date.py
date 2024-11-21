cd4cd58e0fbc0c24cbcd1acbac47d424f8b1a102
Cxx-mlr <spapiernik12@gmail.com>
2024-11-21 16:28:35 -0500

    :sparkles: Add Date class

--- src/Module_1_Foundations/Labs_1_to_5/Lab_2/Date.py
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_2/Date.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import date
+import datetime
 
 class Date:
     def __init__(self, day: int = 1, month: int = 1, year: int = 2000) -> Date:
@@ -25,6 +25,19 @@ class Date:
 
     def get_year(self) -> int:
         return self.__year
+    
+    def __repr__(self) -> str:
+        return (
+            f"Date("
+            f"day={self.__day}, "
+            f"month={self.__month}, "
+            f"year={self.__year}"
+            f")"
+        )
 
     def __str__(self) -> str:
-        return date(year=self.__year, month=self.__month, day=self.__day).strftime("%d-%m-%y")
+        return datetime.date(
+            year=self.__year,
+            month=self.__month,
+            day=self.__day
+        ).strftime("%d-%m-%y")
