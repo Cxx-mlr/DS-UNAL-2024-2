3cdda595cba9a550d3dce2c67a1d62a32b109a20
Cxx-mlr <spapiernik12@gmail.com>
2024-12-02 01:07:15 -0500

    :sparkles: Add Program_2 to initialize Agenda from file, display users, delete by ID, and export to file

---
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_3/Program_2.py
@@ -0,0 +1,15 @@
+from rich import print
+
+from Agenda import Agenda
+
+def main():
+    agenda = Agenda(capacity=10)
+    agenda.load_from_file()
+
+    print(f"{agenda!s}")
+
+    agenda.delete_user(user_id=2345934)
+    agenda.save_to_file(filename="agenda2.txt")
+
+if __name__ == "__main__":
+    main()
