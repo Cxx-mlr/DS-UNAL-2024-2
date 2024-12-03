8b0914c5df2b432fb6a8da7884072a2f3084acf6
Santiago Papiernik <spapiernik12@gmail.com>
2024-12-02 19:55:15 -0500

    :sparkles: Add Lab 4 Implementation with Core Classes and Programs (#15)
    * :sparkles: Add Adress class
    
    * :sparkles: Add Date class
    
    * :sparkles: Add User class
    
    * :sparkles: Add Node class
    
    * :sparkles: Add DoubleNode class
    
    * :sparkles: Add List class
    
    * :sparkles: Add DoubleList class
    
    * :sparkles: Add Program_1
    
    * :sparkles: Add Program_2

---
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_4/DoubleNode.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from typing_extensions import Optional
+
+class DoubleNode:
+    def __init__(self, data: Optional[object] = None) -> DoubleNode:
+        self.__prev: Optional[DoubleNode] = None
+        self.__next: Optional[DoubleNode] = None
+        self.__data = data
+
+    def set_data(self, new_data: object):
+        self.__data == new_data
+
+    def get_data(self) -> object:
+        return self.__data
+    
+    def set_prev(self, prev: DoubleNode):
+        self.__prev = prev
+
+    def get_prev(self) -> DoubleNode:
+        return self.__prev
+    
+    def set_next(self, next: DoubleNode):
+        self.__next = next
+
+    def get_next(self) -> DoubleNode:
+        return self.__next
+
+    def __repr__(self) -> str:
+        return (
+            f"Node("
+            f"data={self.__data!r}, "
+            # f"prev={self.__prev}"
+            f"next={self.__next}"
+            f")"
+        )
