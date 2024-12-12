6aaa038daa3d87af2169527ff1752edac9a569aa
Santiago Papiernik <spapiernik12@gmail.com>
2024-12-12 10:42:55 -0500

    Add classes for agenda management system and initial programs (#19)
    * :sparkles: Add User class
    
    * :sparkles: Add Date class
    
    * :sparkles: Add Address class
    
    * :sparkles: Add Agenda class
    
    * :sparkles: Add Node class
    
    * :sparkles: Add DoubleNode class
    
    * :sparkles: Add List class
    
    * :sparkles: Add DoubleList class
    
    * :sparkles: Add Sorter class
    
    * :sparkles: Add SorterList class
    
    * :sparkles: Add SorterAgenda class
    
    * :poop: Add dead code
    
    * :sparkles: Add Program 1
    
    * :sparkles: Add Program 2
    
    * :sparkles: Add Program 3
    
    * :sparkles: Add agenda.txt file

---
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_5/Agenda.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+from User import User
+
+from typing import List, Optional
+from pathlib import Path
+
+class Agenda:
+    def __init__(self, capacity: int) -> Agenda:
+        self.__capacity: int = capacity
+
+        self.__user_list: List[Optional[User]] = [None for _ in range(self.__capacity)]
+        self.__registered_count: int = 0
+
+    def add_user(self, user: User) -> bool:
+        if self.__registered_count >= self.__capacity:
+            return False
+        
+        if self.find_user(user.get_id()) != -1:
+            return False
+        
+        for index, registered_user in enumerate(self.__user_list):
+            if registered_user is None:
+                self.__user_list[index] = user
+                break
+
+        self.__registered_count += 1
+        return True
+
+    def find_user(self, user_id: int) -> int:
+        for index, user in enumerate(self.__user_list):
+            if user is None:
+                break
+            if user.get_id() == user_id:
+                return index
+        return -1
+
+    def delete_user(self, user_id: int) -> bool:
+        index = self.find_user(user_id)
+        if index == -1:
+            return False
+
+        self.__user_list[index] = None
+        for next_index in range(index + 1, self.__capacity):
+            self.__user_list[index] = self.__user_list[next_index]
+            self.__user_list[next_index] = None
+        
+        self.__registered_count -= 1
+        return True
+    
+    def get_users(self) -> List[User]:
+        return self.__user_list
+
+    def save_to_file(self, filename: str = "agenda.txt"):
+        data = [registered_user.to_csv() for registered_user in self.__user_list[:self.__registered_count]]
+        with open(Path(__file__).parent / filename, "wt", encoding="utf-8") as file:
+            file.writelines(
+                data
+            )
+
+    def load_from_file(self, filename: str = "agenda.txt"):
+        try:
+            with open(Path(__file__).parent / filename, "rt", encoding="utf-8") as file:
+                for csv_string in file:
+                    user = User.from_csv(csv_string=csv_string.strip())
+                    self.add_user(user)
+        except FileNotFoundError:
+            print("File not found. Failed to load data.")
+
+    def __repr__(self) -> str:
+        return (
+            f"Agenda("
+            f"capacity={self.__capacity!r}, "
+            f"registered_count={self.__registered_count!r}, "
+            f"users={self.__user_list!r}"
+            f")"
+        )
+
+    def __str__(self) -> str:
+        users_str = "".join([f"\n{user!s}\n" for user in self.__user_list[:self.__registered_count]])
+        return (
+            f"[green]Agenda Capacity:[/] {self.__capacity}" \
+            f"\n[green]Number of Registered Users:[/] {self.__registered_count}" \
+            f"\n\n[green]Users:[/]\n{users_str}"
+        )
---
+++ src/Module_1_Foundations/Labs_1_to_5/Lab_5/SorterAgenda.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from rich import print
+
+from typing_extensions import Self
+from DoubleList import DoubleList
+from User import User
+
+class SorterAgenda:
+    def __init__(self) -> Self:
+        self.__double_list: DoubleList = DoubleList()
+
+    def add_user(self, user: User) -> None:
+        self.__double_list.add_last(user)
+
+    def sort(self) -> None:
+        for _ in range(self.__double_list.size()):
+            current = self.__double_list.first()
+            while current and current.get_next():
+                next_node = current.get_next()
+                if current.get_data().get_id() > next_node.get_data().get_id():
+                    current_data = current.get_data()
+                    current.set_data(next_node.get_data())
+                    next_node.set_data(current_data)
+                current = next_node
+
+    def __str__(self) -> str:
+        return f"{self.__double_list}"
+    
+    def display(self):
+        current_node = self.__double_list.first()
+        while current_node:
+            user = current_node.get_data()
+            print(
+                f"[yellow bold]Nombre:[/] {user.get_name()} | "
+                f"[yellow bold]ID:[/] {user.get_id()} | "
+                # f"[yellow bold]Fecha de nacimiento:[/] {user.get_birth_date()} | "
+                # f"[yellow bold]Ciudad de nacimiento:[/] {user.get_birth_city()} | "
+                # f"[yellow bold]Teléfono:[/] {user.get_phone_number()} | "
+                # f"[yellow bold]Email:[/] {user.get_email()} | "
+                # f"[yellow bold]Dirección:[/] {user.get_address()}"
+            )
+            current_node = current_node.get_next()
