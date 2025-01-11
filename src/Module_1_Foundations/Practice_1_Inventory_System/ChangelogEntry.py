535ec3626ab5afd05258c2f8d68f08e09faa6692
Cxx-mlr <spapiernik12@gmail.com>
2025-01-10 22:06:47 -0500

    :recycle: Refactor ChangelogEntry class

--- src/Module_1_Foundations/Practice_1_Inventory_System/ChangelogEntry.py
+++ src/Module_1_Foundations/Practice_1_Inventory_System/ChangelogEntry.py
@@ -1,76 +1,169 @@
 from __future__ import annotations
 
-from typing_extensions import Literal
+from typing_extensions import TYPE_CHECKING, Literal, Optional
 
-from InventoryItem import InventoryItem
-from Date import Date
-from Time import Time
+if TYPE_CHECKING:
+    from Equipment import Equipment
+    from Request import Request
+
+from Agenda import Agenda
+from Inventory import Inventory
+from User import User
+from RequestList import RequestList
+from DateTime import DateTime
+
+from config import (
+    EMPLOYEES_PATH,
+    SAVED_INVENTORY_PATH,
+    DELETED_INVENTORY_PATH,
+    ADD_REQUESTS_PATH,
+    DELETE_REQUESTS_PATH,
+)
+
 
 class ChangelogEntry:
     def __init__(
-            self,
-            user_id: int,
-            serial_number: int,
-            what: Literal["agregar", "eliminar"],
-            date: Date,
-            time: Time,
-            status: str = "PENDING"
+        self,
+        user_id: int,
+        serial_number: int,
+        action: Literal["agregar", "eliminar"],
+        date_time: DateTime,
+        status: Literal["PENDING", "APPROVED", "REJECTED"] = "PENDING",
     ) -> ChangelogEntry:
+        self.__status: Optional[Literal["agregar", "eliminar"]] = None
         self.__user_id = int(user_id)
         self.__serial_number = serial_number
-        self.__what = what
-        self.__date = date
-        self.__time = time
+        self.__action = action
+        self.__date_time = date_time
         self.__status = status
 
-    @classmethod
-    def from_item(cls, item: InventoryItem, what: Literal["agregar", "eliminar"]) -> ChangelogEntry:
-        return cls(
-            user_id=item.get_user_id(),
-            serial_number=item.get_serial_number(),
-            what=what,
-            date=Date.now(),
-            time=Time.now()
+    def get_request(self) -> Request:
+        from Request import Request
+
+        user = self.get_user()
+        equipment = self.get_equipment()
+
+        request = Request(
+            username=user.get_name(),
+            user_id=self.get_user_id(),
+            name=equipment.get_name(),
+            serial_number=equipment.get_serial_number(),
+            purchase_date=equipment.get_purchase_date(),
+            price=equipment.get_price(),
         )
+        request.set_action(self.get_action())
+        return request
+
+    def get_user(self) -> User:
+        with Agenda(filename=EMPLOYEES_PATH) as users:
+            node = users.find_if(lambda user: user.get_id() == self.get_user_id())
+
+            if node is None:
+                return User(id=self.get_user_id())
+
+            return node.get_data()
+
+    def get_equipment(self) -> Equipment:
+        with (
+            Inventory(filename=SAVED_INVENTORY_PATH) as saved_inventory,
+            Inventory(filename=DELETED_INVENTORY_PATH) as deleted_inventory,
+            RequestList(filename=ADD_REQUESTS_PATH) as add_requests,
+            RequestList(filename=DELETE_REQUESTS_PATH) as remove_requests,
+        ):
+            node = (
+                saved_inventory.find_if(
+                    lambda item: item.get_serial_number() == self.get_serial_number()
+                )
+                or deleted_inventory.find_if(
+                    lambda item: item.get_serial_number() == self.get_serial_number()
+                )
+                or add_requests.find_if(
+                    lambda request: (
+                        request.get_serial_number() == self.get_serial_number()
+                        and request.get_user_id() == self.get_user_id()
+                    )
+                )
+                or remove_requests.find_if(
+                    lambda request: (
+                        request.get_serial_number() == self.get_serial_number()
+                        and request.get_user_id() == self.get_user_id()
+                    )
+                )
+            )
+
+            if node is None:
+                return Equipment(serial_number=self.get_serial_number())
+
+            return node.get_data().get_equipment()
 
     def get_user_id(self) -> int:
         return self.__user_id
-    
+
     def get_serial_number(self) -> int:
         return self.__serial_number
-    
-    def get_action(self) -> str:
-        return self.__what
-    
-    def get_date(self) -> Date:
-        return self.__date
-    
-    def get_time(self) -> Time:
-        return self.__time
-    
-    def get_status(self) -> str:
+
+    def get_action(self) -> Literal["agregar", "eliminar"]:
+        return self.__action
+
+    def set_action(self, action: Literal["agregar", "eliminar"]):
+        self.__action = action
+
+    def get_date_time(self) -> DateTime:
+        return self.__date_time
+
+    def set_date_time(self, date_time: DateTime):
+        self.__date_time = date_time
+
+    def get_status(self) -> Literal["PENDING", "APPROVED", "REJECTED"]:
         return self.__status
 
+    def set_status(self, status: Literal["PENDING", "APPROVED", "REJECTED"]):
+        self.__status = status
+
     def to_csv(self) -> str:
-        return f"{self.__user_id} {self.__serial_number} {self.__what} {self.__date.to_csv()} {self.__time.to_csv()}"
-    
+        return f"{self.__user_id} {self.__serial_number} {self.__action.capitalize()} {self.__date_time.to_csv()}"
+
     @staticmethod
     def from_csv(csv_string: str) -> ChangelogEntry:
-        parts = csv_string.split(" ")
-        user_id = int(parts[0])
-        serial_number = int(parts[1])
-        what = parts[2]
+        parts = csv_string.strip().split()
+        if len(parts) != 9:
+            raise ValueError(
+                f"La cadena CSV debe contener exactamente 9 partes separadas por espacios, pero se obtuvieron {len(parts)} partes: {parts}"
+            )
 
-        date = Date.from_csv(" ".join(parts[3:6]))
-        time = Time.from_csv(" ".join(parts[6:9]))
+        try:
+            user_id = int(parts[0])
+        except ValueError:
+            raise ValueError(
+                f"No se pudo convertir 'user_id' a un entero. Valor proporcionado: {parts[0]}"
+            )
+
+        try:
+            serial_number = int(parts[1])
+        except ValueError:
+            raise ValueError(
+                f"No se pudo convertir 'serial_number' a un entero. Valor proporcionado: {parts[1]}"
+            )
+
+        action = parts[2].lower()
+        if action not in ["agregar", "eliminar"]:
+            raise ValueError(
+                f"El valor de 'action' debe ser 'agregar' o 'eliminar'. Valor proporcionado: {action}"
+            )
+
+        try:
+            date_time = DateTime.from_csv(" ".join(parts[3:9]))
+        except Exception as e:
+            raise ValueError(
+                f"No se pudo instanciar 'DateTime' de las partes {parts[3:9]}. Error: {e}"
+            )
 
         return ChangelogEntry(
             user_id=user_id,
             serial_number=serial_number,
-            what=what,
-            date=date,
-            time=time
+            action=action,
+            date_time=date_time,
         )
-    
+
     def __str__(self) -> str:
         return self.to_csv()
