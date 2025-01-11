f5f11e30a4f7c0a6205075991f837a9ea34d9c76
Cxx-mlr <spapiernik12@gmail.com>
2025-01-10 22:14:09 -0500

    :recycle: Refactor config.py file

--- src/Module_1_Foundations/Practice_1_Inventory_System/config.py
+++ src/Module_1_Foundations/Practice_1_Inventory_System/config.py
@@ -1,5 +1,14 @@
 from pathlib import Path
 
 THIS_PATH = Path(__file__).parent
-READ_PATH = THIS_PATH / "data"
-WRITE_PATH = THIS_PATH / "data_output"
+DATA_PATH = THIS_PATH / "data"
+
+DELETE_REQUESTS_PATH = DATA_PATH / "delete_requests.txt"
+ADD_REQUESTS_PATH = DATA_PATH / "add_requests.txt"
+PASSWORDS_PATH = DATA_PATH / "passwords.txt"
+SAVED_INVENTORY_PATH = DATA_PATH / "saved_inventory.txt"
+DELETED_INVENTORY_PATH = DATA_PATH / "deleted_inventory.txt"
+EMPLOYEES_PATH = DATA_PATH / "employees.txt"
+APPROVED_CHANGE_CONTROL_PATH = DATA_PATH / "approved_change_control.txt"
+REJECTED_CHANGE_CONTROL_PATH = DATA_PATH / "rejected_change_control.txt"
+REQUESTS_STATUS_FILENAME = "requests_status {} {}.txt"
