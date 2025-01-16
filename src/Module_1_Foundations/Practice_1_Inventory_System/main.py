3e5799660aa7ff1ed9f7d218e4dea6798e251d35
Cxx-mlr <spapiernik12@gmail.com>
2025-01-15 23:33:51 -0500

    :truck: Move authentication logic to `auth.py`

--- src/Module_1_Foundations/Practice_1_Inventory_System/main.py
+++ src/Module_1_Foundations/Practice_1_Inventory_System/main.py
@@ -1,68 +1,7 @@
 from __future__ import annotations
-from rich import print
-
-from typing_extensions import Optional, Union
-
-from config import PASSWORDS_PATH, EMPLOYEES_PATH
-from utils import ask_integer, ask_string
-
-from UserList import UserList
-from CredentialsList import CredentialsList
-from Researcher import Researcher
-from Administrator import Administrator
 
 import os
-
-
-def authenticate(
-    user_list: UserList, user_id: int, password: str
-) -> Optional[Union[Researcher, Administrator]]:
-    with CredentialsList(filename=PASSWORDS_PATH) as credentials_list:
-        credentials_node = credentials_list.find_if(
-            lambda credentials: credentials.get_user_id() == user_id
-        )
-
-    if credentials_node is None:
-        return None
-    else:
-        credentials = credentials_node.get_data()
-
-    if not credentials.get_password() == password:
-        return None
-
-    user_node = user_list.find_if(lambda user: user.get_id() == credentials.get_user_id())
-
-    if user_node is None:
-        return None
-
-    else:
-        user = user_node.get_data()
-
-    if credentials.is_researcher():
-        return Researcher(user)
-
-    elif credentials.is_administrator():
-        return Administrator(user)
-
-
-def login(
-    user_id: Optional[int] = None, password: Optional[str] = None
-) -> Optional[Union[Researcher, Administrator]]:
-    user_list = UserList(capacity=13)
-    user_list.load_from_file(filename=EMPLOYEES_PATH)
-
-    if user_id is None:
-        user_id = ask_integer("ID: ", "Por favor, proporcione un ID de usuario válido.")
-    if password is None:
-        password = ask_string("Contraseña: ")
-
-    session = authenticate(user_list, user_id, password)
-
-    if not session:
-        print("Usuario o contraseña incorrectos.")
-        exit()
-
-    return session
+from auth import login
 
 
 def main():
