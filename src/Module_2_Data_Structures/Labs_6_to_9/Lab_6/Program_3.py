ef67d5f488b28c66bb7d005359c8cf84146d6242
Cxx-mlr <spapiernik12@gmail.com>
2025-02-06 23:55:21 -0500

    :recycle: Enhance script flow with user input pause

--- src/Module_2_Data_Structures/Labs_6_to_9/Lab_6/Program_3.py
+++ src/Module_2_Data_Structures/Labs_6_to_9/Lab_6/Program_3.py
@@ -9,12 +9,13 @@ def main():
     user_queue.register(User(name="Carlos-Rodriguez", id=67890234))
     user_queue.register(User(name="Mariana-Lopez", id=56789012))
     user_queue.register(User(name="Andres-Valencia", id=34567891))
+    user_queue.save_to_file()
 
-    print(user_queue)
+    input("Presiona Enter para continuar...")
+
     user_queue.serve_next()
     user_queue.serve_next()
     user_queue.save_to_file()
-    print(user_queue)
 
 if __name__ == "__main__":
     main()
